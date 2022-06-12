65cca78c7f2e82a6c0a7dfe050bc41936e21d552
ammar <ammarraneez@gmail.com>
2022-06-12 16:08:30 +0530

    feat: display clients base

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+import Clients from './components/Clients';
 import Header from './components/Header';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
     <Header />
       <div className="container">
-        <h1>Hello World!</h1>
+        <Clients />
       </div>
-    </>
+    </ApolloProvider>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('colors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
@@ -10,6 +11,7 @@ const app = express();
 
 connectDB();
 
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: process.env.NODE_ENV === 'development',
