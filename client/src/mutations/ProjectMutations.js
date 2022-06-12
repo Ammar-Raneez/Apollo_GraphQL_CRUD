323db8a8bdcd961bfe8db4286a60444b36f183be
ammar <ammarraneez@gmail.com>
2022-06-12 17:05:28 +0530

    feat: delete project

--- client/src/mutations/ProjectMutations.js
+++ client/src/mutations/ProjectMutations.js
@@ -15,3 +15,11 @@ export const ADD_PROJECT = gql`
     }
   }
 `;
+
+export const DELETE_PROJECT = gql`
+  mutation deleteProject($id: ID!) {
+    deleteProject(id: $id) {
+      name
+    }
+  }
+`;
