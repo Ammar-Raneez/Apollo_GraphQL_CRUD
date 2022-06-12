c031056fbe08517baf39fb9dde9bbc956dcd9c03
ammar <ammarraneez@gmail.com>
2022-06-12 16:59:08 +0530

    feat: add project

--- client/src/components/AddProjectModal.jsx
+++ client/src/components/AddProjectModal.jsx
@@ -1,14 +1,27 @@
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { useState } from 'react';
 import { FaList } from 'react-icons/fa';
 
+import { ADD_PROJECT } from '../mutations/ProjectMutations';
+import { GET_PROJECTS } from '../queries/ProjectQueries';
 import { GET_CLIENTS } from '../queries/ClientQueries';
 
-export default function AddClientModal() {
+export default function AddProjectModal() {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [clientId, setClientId] = useState('');
   const [status, setStatus] = useState('new');
+
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: { name, description, clientId, status },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS });
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: [...projects, addProject] },
+      });
+    },
+  });
 
   // Get Clients for select
   const { loading, error, data } = useQuery(GET_CLIENTS);
@@ -16,9 +29,11 @@ export default function AddClientModal() {
   const onSubmit = (e) => {
     e.preventDefault();
 
-    if (name === '' || description === '' || status === '') {
+    if (name === '' || description === '' || status === '' || clientId === '') {
       return alert('Please fill in all fields');
     }
+
+    addProject(name, description, clientId, status);
 
     setName('');
     setDescription('');
---
+++ client/src/mutations/ProjectMutations.js
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client';
+
+export const ADD_PROJECT = gql`
+  mutation addProject($name: String!, $description: String!, $clientId: ID!, $status: ProjectStatus!) {
+    addProject(name: $name, description: $description, clientId: $clientId, status: $status) {
+      id
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
