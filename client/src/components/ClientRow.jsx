4c3b7a431cedebc4f8aa84ceaa5ff358e1c5ecf7
ammar <ammarraneez@gmail.com>
2022-06-12 17:14:17 +0530

    Cascade delete project on client delete

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,6 +2,7 @@ import { useMutation } from '@apollo/client';
 import { FaTrash } from 'react-icons/fa';
 import { DELETE_CLIENTS } from '../mutations/ClientMutations';
 import { GET_CLIENTS } from '../queries/ClientQueries';
+import { GET_PROJECTS } from '../queries/ProjectQueries';
 
 
 export default function ClientRow({ client }) {
@@ -10,18 +11,18 @@ export default function ClientRow({ client }) {
     variables: { id: client.id },
 
     // If there are multiple requests that will occur updating the cache is the better approach
-    // refetchQueries: [{ query: GET_CLIENTS }],
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],    // on client delete all associated projects are also removed, hence refetch to update
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
 
-      // Update the cache with clients apart from the one just deleted
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      });
-    },
+    //   // Update the cache with clients apart from the one just deleted
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   });
+    // },
   });
 
   return (
