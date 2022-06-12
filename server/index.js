17073b62a6a8145edb78b98bcbed0b0c5371c088
ammar <ammarraneez@gmail.com>
2022-06-12 10:04:06 +0530

    feat: connect to db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+require('colors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 
 const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: process.env.NODE_ENV === 'development',
